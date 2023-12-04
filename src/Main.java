18893bccb842ce44c1acd6f0c34a8b4be0852002
biplab-w <waglebip@gmail.com>
2023-12-04 08:34:13 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-9.58549));
+        System.out.println(Math.round(3.222));
+        System.out.println(Math.round(4.56));
+        System.out.println(Math.ceil(3.11));
+        System.out.println(Math.max(32, 400));
+        System.out.println(Math.min(600,30));
+        System.out.println(Math.signum(34.234f));
+        System.out.println(Math.pow(2,3));
+
+        System.out.println(Math.sqrt(81));
+        System.out.println(Math.random());
+        System.out.println(Math.log(10));
+        System.out.println(Math.log10(10));
+    }
+
+}
